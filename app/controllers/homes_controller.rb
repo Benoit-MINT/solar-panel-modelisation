c681609a7398540694a7e5ac49d5044576dcf8d9
Benoit Queyron <queyronbenoit@gmail.com>
2024-04-24 19:10:16 +0200

    form for new home

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -7,4 +7,26 @@ class HomesController < ApplicationController
 
   end
 
+  def new
+    @home_new = Home.new
+  end
+
+  def create
+    @home_new = Home.new(home_params)
+    (1..12).each do |month|
+      @home_new.home_consumption_months[month - 1] = params[:home][:home_consumption_months]["#{month}"].to_f
+    end
+    if @home_new.save
+      redirect_to homes_path, alert: "Le projet est créé"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def home_params
+    params.require(:home).permit(:address)
+  end
+
 end
