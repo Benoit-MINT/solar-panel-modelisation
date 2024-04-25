8daa4adec7722634e5500d43857ebf129fb01a73
Benoit Queyron <queyronbenoit@gmail.com>
2024-04-25 16:48:48 +0200

    production, self, back calculation energy

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_25_101042) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_25_125234) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,8 +21,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_25_101042) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "home_consumption_months", default: [], array: true
-    t.integer "self_consumption_months", default: [], array: true
-    t.integer "back_energy_months", default: [], array: true
   end
 
   create_table "photovoltaics", force: :cascade do |t|
@@ -32,6 +30,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_25_101042) do
     t.datetime "updated_at", null: false
     t.integer "ratio_months", default: [], array: true
     t.integer "production_months", default: [], array: true
+    t.integer "self_consumption_months", default: [], array: true
+    t.integer "back_energy_months", default: [], array: true
     t.index ["home_id"], name: "index_photovoltaics_on_home_id"
   end
 
