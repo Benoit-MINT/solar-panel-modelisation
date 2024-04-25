74dbddd3c4de4d96fabb7cc1ded016338481b299
Benoit Queyron <queyronbenoit@gmail.com>
2024-04-25 17:14:08 +0200

    edit home project

--- app/models/home.rb
+++ app/models/home.rb
@@ -1,6 +1,7 @@
 class Home < ApplicationRecord
   has_many :photovoltaics
 
+  validates :name, presence: :true
   validates :address, presence: :true
   validates :home_consumption_months, presence: true
   validates :home_consumption_months, length: { is: 12 }
