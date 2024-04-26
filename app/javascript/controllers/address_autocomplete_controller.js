7a4a3423b59dc59a6f80e939b2226d2f5d542ac7
Benoit Queyron <queyronbenoit@gmail.com>
2024-04-26 16:39:54 +0200

    autocomplete address

--- app/javascript/controllers/address_autocomplete_controller.js
+++ app/javascript/controllers/address_autocomplete_controller.js
@@ -2,21 +2,30 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="address-autocomplete"
 export default class extends Controller {
-  static targets = ["address"]
+  static targets = ["address", "suggestions"]
 
   connect() {
   }
 
-  search(event) {
-    var result = []
+  search() {
+    this.suggestionsTarget.innerHTML = '';
     var address = this.addressTarget.value.replace(/[, ]+/g, '+');
-    var url = `https://nominatim.openstreetmap.org/search?q=${address}&format=json&countrycodes=fr`
-    console.log(url)
-    fetch(url)
-    .then(response => response.json())
-    .then((data) => {
-      console.log(data[0].display_name);
-    });
+    if (address.length > 1) {
+      var url = `https://nominatim.openstreetmap.org/search?q=${address}&format=json&countrycodes=fr`
+      fetch(url)
+      .then(response => response.json())
+      .then((data) => {
+        data.slice(0, 5).forEach((suggestion) => {
+          this.suggestionsTarget.insertAdjacentHTML("beforeend", `<li data-action="click->address-autocomplete#select">${suggestion.display_name}</li>`);
+        });
+      });
+    }
+  }
 
+  select(event) {
+    console.log(event.target.innerText)
+    console.log(this.addressTarget.value)
+    this.addressTarget.value = event.target.innerText
+    this.suggestionsTarget.innerHTML = '';
   }
 }
