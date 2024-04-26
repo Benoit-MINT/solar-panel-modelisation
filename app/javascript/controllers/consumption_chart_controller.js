a8077ed7efa22f9b96f3a21dd34f7a1b0ffb0e63
Benoit Queyron <queyronbenoit@gmail.com>
2024-04-26 09:28:32 +0200

    first version of home consumption graph

--- app/javascript/controllers/consumption_chart_controller.js
+++ app/javascript/controllers/consumption_chart_controller.js
@@ -3,7 +3,26 @@ import { Chart } from "chart.js";
 
 // Connects to data-controller="consumption-chart"
 export default class extends Controller {
+  static targets = ["consumptionHome"]
+
   connect() {
-    console.log("hello from consumption chart controller JS")
+    const labels = ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre']
+    var consumption_data = this.consumptionHomeTarget.dataset.value
+    var consumption__data_json = JSON.parse(consumption_data)
+
+    new Chart(this.element, {
+      type: 'bar',
+      data: {
+        labels: labels,
+        datasets: [{
+          label: 'Consommation mensuelle (kW/h)',
+          data: consumption__data_json,
+          backgroundColor: [
+          'rgb(54, 162, 235)',
+          ],
+          hoverOffset: 4
+        }]
+      }
+    });
   }
 }
