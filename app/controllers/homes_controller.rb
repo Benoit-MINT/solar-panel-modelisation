e46fe1386dd57297efdf9a909c28091613a2d6f5
Benoit Queyron <queyronbenoit@gmail.com>
2024-04-28 11:50:05 +0200

    transfert vers le model

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -19,14 +19,14 @@ class HomesController < ApplicationController
     @home_new = Home.new(home_params)
 
     if params[:home][:file].present?
-      handle_uploaded_csv(@home_new, params[:home][:file])
+      @home_new.handle_uploaded_csv(params[:home][:file])
     else
       (1..12).each do |month|
         @home_new.home_consumption_months[month - 1] = params[:home][:home_consumption_months]["#{month}"].to_i
       end
     end
 
-    price_consumption_calculation(@home_new)
+    @home_new.price_consumption_calculation
 
     if @home_new.save
       redirect_to homes_path, alert: "Le projet est créé"
@@ -42,10 +42,17 @@ class HomesController < ApplicationController
   def update
     @home = Home.find(params[:id])
     @home.update(home_params)
-    (1..12).each do |month|
-      @home.home_consumption_months[month - 1] = params[:home][:home_consumption_months]["#{month}"].to_f
+
+    if params[:home][:file].present?
+      @home.handle_uploaded_csv(params[:home][:file])
+    else
+      (1..12).each do |month|
+        @home.home_consumption_months[month - 1] = params[:home][:home_consumption_months]["#{month}"].to_f
+      end
     end
-    price_consumption_calculation(@home)
+
+    @home.price_consumption_calculation
+
     @home.save
     redirect_to home_path(@home), alert: "Projet actualisé!"
   end
@@ -62,20 +69,20 @@ class HomesController < ApplicationController
     params.require(:home).permit(:name, :address, :buy_price_electricity, :sale_price_electricity)
   end
 
-  def handle_uploaded_csv(home, file)
-    linky_data = []
-    CSV.foreach(file, encoding: 'utf-8') do |row|
-      linky_data << row
-    end
-    filtered_data = linky_data.select { |row| row[0]&.start_with?(/^\d/) }
-    filtered_data.map! { |row| row[0].split(';').slice(0, 2) }
-    filtered_data.select! { |row| row[0].include?("2023") }
-    home.home_consumption_months = filtered_data.map { |row| row[1].to_i }.reverse
-  end
+  # def handle_uploaded_csv(home, file)
+  #   linky_data = []
+  #   CSV.foreach(file, encoding: 'utf-8') do |row|
+  #     linky_data << row
+  #   end
+  #   filtered_data = linky_data.select { |row| row[0]&.start_with?(/^\d/) }
+  #   filtered_data.map! { |row| row[0].split(';').slice(0, 2) }
+  #   filtered_data.select! { |row| row[0].include?("2023") }
+  #   home.home_consumption_months = filtered_data.map { |row| row[1].to_i }.reverse
+  # end
 
-  def price_consumption_calculation(home)
-    (0..11).each do |month|
-      home.price_consumption_months[month] = (home.home_consumption_months[month] * home.buy_price_electricity).round(2)
-    end
-  end
+  # def price_consumption_calculation(home)
+  #   (0..11).each do |month|
+  #     home.price_consumption_months[month] = (home.home_consumption_months[month] * home.buy_price_electricity).round(2)
+  #   end
+  # end
 end
--- app/models/home.rb
+++ app/models/home.rb
@@ -14,6 +14,23 @@ class Home < ApplicationRecord
 
   after_update :update_photovoltaics
 
+  def handle_uploaded_csv(file)
+    linky_data = []
+    CSV.foreach(file, encoding: 'utf-8') do |row|
+      linky_data << row
+    end
+    filtered_data = linky_data.select { |row| row[0]&.start_with?(/^\d/) }
+    filtered_data.map! { |row| row[0].split(';').slice(0, 2) }
+    filtered_data.select! { |row| row[0].include?("2023") }
+    self.home_consumption_months = filtered_data.map { |row| row[1].to_i }.reverse
+  end
+
+  def price_consumption_calculation
+    (0..11).each do |month|
+      self.price_consumption_months[month] = (self.home_consumption_months[month] * self.buy_price_electricity).round(2)
+    end
+  end
+
   private
   def update_photovoltaics
     photovoltaics.each(&:update_attributes_dependent_on_home)
