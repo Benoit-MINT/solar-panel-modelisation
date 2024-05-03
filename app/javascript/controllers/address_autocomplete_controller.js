db90a7a1b55995143f2f5ac52cf92b0f4599db77
Benoit Queyron <queyronbenoit@gmail.com>
2024-05-03 15:00:04 +0200

    passage sur api gouv adresse

--- app/javascript/controllers/address_autocomplete_controller.js
+++ app/javascript/controllers/address_autocomplete_controller.js
@@ -2,36 +2,45 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="address-autocomplete"
 export default class extends Controller {
-  static targets = ["address", "latlon", "map"]
+  static targets = ["address", "latlon", "map", "addressList"]
 
   connect() {
+    // on initialise une map
     this.map(48.866667, 2.333333, 4)
   }
 
-  search(event) {
-    const datalist = document.getElementById("address_list")
-    datalist.innerHTML = '';
-    var address = this.addressTarget.value.replace(/[, ]+/g, '+');
-    if (address.length > 2) {
-      var url = `https://nominatim.openstreetmap.org/search?q=${address}&format=json&countrycodes=fr`
+  search() {
+    this.addressListTarget.innerHTML = '';
+    var addressSearch = this.addressTarget.value.replace(/[, ]+/g, '+');
+    if (addressSearch.length > 4) {
+      var url = `https://api-adresse.data.gouv.fr/search/?q=${addressSearch}&limit=5`
       fetch(url)
       .then(response => response.json())
       .then((data) => {
-        data.slice(0, 3).forEach((suggestion) => {
-          // datalist.insertAdjacentHTML("beforeend", `<option value="${suggestion.display_name}" data-address-autocomplete-target="latlon" data-value = "[${suggestion.lat}, ${suggestion.lon}, "${suggestion.display_name}"]">`);
-          datalist.insertAdjacentHTML("beforeend", `<li data-action="click->address-autocomplete#select" data-address-autocomplete-target="latlon" data-value = "[${suggestion.lat}, ${suggestion.lon}]">${suggestion.display_name}</li>`);
+        data.features.forEach((suggestion) => {
+          var label = suggestion.properties.label
+          var lat = suggestion.geometry.coordinates[1]
+          var lon = suggestion.geometry.coordinates[0]
+          this.addressListTarget.insertAdjacentHTML("beforeend", `<li data-action="click->address-autocomplete#select" data-address-autocomplete-target="latlon" data-value = "[${lat}, ${lon}]">${label}</li>`);
         });
+        // data.slice(0, 3).forEach((suggestion) => {
+        //   // ancien : datalist.insertAdjacentHTML("beforeend", `<option value="${suggestion.display_name}" data-address-autocomplete-target="latlon" data-value = "[${suggestion.lat}, ${suggestion.lon}, "${suggestion.display_name}"]">`);
+        //   // good : this.addressListTarget.insertAdjacentHTML("beforeend", `<li data-action="click->address-autocomplete#select" data-address-autocomplete-target="latlon" data-value = "[${suggestion.lat}, ${suggestion.lon}]">${suggestion.display_name}</li>`);
+        // });
       });
     }
   }
 
   select(event) {
-    this.addressTarget.value = event.srcElement.innerText
-    console.log(this.addressTarget.value)
+    this.addressTarget.value = event.target.innerText
+    // console.log(this.addressTarget.value)
+
     const latlon = this.latlonTarget.dataset.value
     const latlon_data = JSON.parse(latlon)
-    console.log(latlon_data)
+    // console.log(latlon_data)
     this.map(latlon_data[0], latlon_data[1], 15)
+
+    this.addressListTarget.innerHTML = ''
   }
 
   map(lat, lon, zoom) {
